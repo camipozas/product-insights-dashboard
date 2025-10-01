d49b3a3cda667b9a20326105c6d4945659a9c671
camipozas <camila.pozas@banca.me>
2025-10-01 11:16:26 -0300

    refactor Tailwind CSS configuration for consistency; update import syntax and format paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
@@ -13,4 +13,3 @@ const config: Config = {
 };
 
 export default config;
-
